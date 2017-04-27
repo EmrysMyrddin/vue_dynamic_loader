2775d15cbb67ff17807570c9d1708cfe172e61c2
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-04-27 22:52:53 +0200

    [Add] Inner global component

---
+++ example/public/js/plugincontainer.component.js
@@ -0,0 +1,14 @@
+/**
+ * Created by gwennael.buchet on 09/01/17.
+ */
+
+let plugincontainer = Vue.component(
+	'plugincontainer',
+	{
+		template: '<div></div>',
+
+		data : function () {
+			return {}
+		}
+	}
+);
